46b79d442d8852efdfc8dc3027028d99759c59ee
Jessie Houghton <JessieHoughtonDesign@gmail.com>
2025-11-10 21:43:44 -0800

    Fix image organization and add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand">Jessie Houghton</Link>
+        <Link to="/" className="navbar-brand">
+          <img src="/Logo/logo.png" alt="Jessie Houghton" className="navbar-logo" />
+          <span className="navbar-brand-text">Jessie Houghton Design</span>
+        </Link>
         <ul className="navbar-menu">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/portfolio">Portfolio</Link></li>
